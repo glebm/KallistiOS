083191ce6ee1d28c149af215369d1d68074bcdcb
Colton Pawielski <cepawiel@mtu.edu>
2023-02-24 17:11:59 -0600

    Modify compiler_tls.c example to work with GCC4

--- examples/dreamcast/basic/threading/compiler_tls/compiler_tls.c
+++ examples/dreamcast/basic/threading/compiler_tls/compiler_tls.c
@@ -4,7 +4,7 @@
 
    (c)2023 Colton Pawielski
 
-   A simple example showing off _Thread_local variables
+   A simple example showing off thread local variables
 
    This example launches two threads that access variables
    placed in the TLS segment by the compiler. The compiler
@@ -16,22 +16,33 @@
 
 #include <kos.h>
 
-_Thread_local uint32_t tbss_test = 0;
-_Thread_local uint32_t tdata_test = 5;
+#if (__GNUC__ <= 4)
+/* GCC4 only supports using TLS with the __thread identifier,
+   even when passed the -std=c99 flag */
+#define thread_local __thread
+#else
+/* Newer versions of GCC use C11's _Thread_local to specify TLS */
+#define thread_local _Thread_local
+#endif
+
+extern thread_local uint32_t tbss_test = 0;
+extern thread_local uint32_t tdata_test = 5;
+
 
 /* Thread Function */
 void *thd(void *v) {
+    int i;
     int id = (int) v;
 
     printf("Started Thread %d\n", id);
 
-    for (int i = 0; i < 5; i++){        
+    for (i = 0; i < 5; i++){        
         printf("Thread[%d]\tbss_test = 0x%lX\n", id, tbss_test);
         tbss_test++;
         thd_sleep(50);
     }
 
-    for (int i = 0; i < 5; i++){
+    for (i = 0; i < 5; i++){
         printf("Thread[%d]\ttdata_test = 0x%lX\n", id, tdata_test);
         tdata_test++;
         thd_sleep(50);
@@ -43,19 +54,19 @@ void *thd(void *v) {
 
 /* The main program */
 int main(int argc, char **argv) {
-    kthread_t * t0, * t1;
-
     const int thread_count = 2;
+
+    int i;
+    kthread_t * threads[thread_count];   
 
     printf("Starting Threads\n");
 
-    kthread_t * threads[thread_count];
-    for(int i = 0; i < thread_count; i++) {
+    for(i = 0; i < thread_count; i++) {
         threads[i] = thd_create(0, thd, (void *) i);
     };
 
-    for(int i = 0; i < thread_count; i++) {
-        uint32_t ret = (uint32_t) thd_join(threads[i], NULL);
+    for(i = 0; i < thread_count; i++) {
+        int ret = (int) thd_join(threads[i], NULL);
         printf("Thread[%d] Returned: %d\n", i, ret);
     }
     
